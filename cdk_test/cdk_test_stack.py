1ad163643a97dd0ab50fbec82aeaf0e5be663302
Nick Padon <npadon@gmail.com>
2024-09-29 13:46:01 -0500

    some commit

--- cdk_test/cdk_test_stack.py
+++ cdk_test/cdk_test_stack.py
@@ -14,5 +14,5 @@ class CdkTestStack(Stack):
 
         queue = sqs.Queue(
             self, "CdkTestQueue",
-            visibility_timeout=Duration.seconds(600),
+            visibility_timeout=Duration.seconds(650),
         )
